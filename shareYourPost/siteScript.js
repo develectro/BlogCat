fed8b93c955d402ccebdfbc83a0916e8ba9dceee
develectro <46164660+develectro@users.noreply.github.com>
2019-07-31 21:55:11 +0200

    Update siteScript.js

--- shareYourPost/siteScript.js
+++ shareYourPost/siteScript.js
@@ -7,14 +7,35 @@ var options = {
     theme: 'snow',
     debug: 'info',
     readOnly: false,
+    placeholder: 'what do you feel now?',
     modules: {
         toolbar: myCustomToolBar
     }
-
 };
 var editor = new Quill('.editor', options);
 
+function getEditorContents() {
+    //console.log(editor.root.innerHTML);
+    // use this method in the case you need to use plain html text.
+}
+var textObject;
+var JSONstring;
+var recievedJSON;
 document.getElementById('subButton').addEventListener('click', clicked => {
-    var dataWithDeltaFormat = quill.getContetnts();
-    var myCookie = document.cookie = dataWithDeltaFormat;
+    textObject = editor.getContents(0);
+    JSONstring = JSON.stringify(textObject);
 });
+
+var ajax = new XMLHttpRequest();
+this.onreadystatechange = function() {
+    if (this.raedyState == 4 && this.status == 200) {
+        recievedJSON = this.JSON.parse(ajax.responseText);
+        var postBoardWindow = this.window.open('file:///C:/Users/INSPIRON/Desktop/shareYourPost/postBoard.html');
+        postBoardWindow.getElementsByClassName('board').innerHTML = this.recievedJSON;
+    } else {
+        var postBoardWindow = this.window.open('file:///C:/Users/INSPIRON/Desktop/shareYourPost/postBoard.html');
+        postBoardWindow.getElementsByClassName('board').innerHTML = this.recievedJSON;
+    }
+}
+ajax.open('POST', '/database.php', true);
+ajax.send(JSONstring);
